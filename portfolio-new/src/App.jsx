a5199afe31aaba37f169db996c566a514d638b4b
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-03-02 04:09:26 +0530

    changes

--- portfolio-new/src/App.jsx
+++ portfolio-new/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='main'>
+      <h1 className='name'>hey, it's Bharath Kumar </h1>
+      <h5 className='first-line'>that 21 year old generalist guy. Engineer.</h5>
+      <h2 className='title'>about</h2>
+      <h5>i write software.</h5>
+      <h5> <span className='curiosity'> fueled by Curiosity.</span></h5>
+      <h5>physics, math, CS, space, and design—I’m hooked.</h5>
+      <h5> code or cosmo, hit me up w a <span> </span>
+        <a href="https://cal.com/bharath-kumar-reddy/30min" target='_blank'> quick video chat</a>
+      </h5> 
+      <h2 className='title'>latest works</h2>
+      <div className='latest-works'>
+        <div className='works-line'>
+          <div className='latest-work-cards'></div>
+          <div className='latest-work-cards'></div>
+        </div>
+        <div className='works-line'>
+          <div className='latest-work-cards'></div>
+          <div className='latest-work-more-card'>More Work</div>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <h2 className='title'>places i worked at</h2>
+      <h2 className='title'>education</h2>
+      <div className='uni'>
+        <div className='uni-icon'> <img src="../Public/dsce-icon-monochrome.svg" alt="" /></div>
+        <div className='uni-mid'>
+        <div className="uni-name">DSCE, Bangalore</div>
+        <div className="uni-major">electronics and telecommunication</div>
+        </div>
+        <div className="uni-year">2021-2025</div>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2 className='title'>skills</h2>
+      <h2 className='title'>socials</h2>
+
+      
+    </main>
+
     </>
   )
 }
